1603ded94f85d6241c7962533872455fa285e460
Nicole <nicolevladislavna@gmail.com>
2024-04-20 15:35:09 +0300

    Initial Commit

---
+++ HW 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.01
+//
+//  Created by Veronika Iskandarova on 20.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
